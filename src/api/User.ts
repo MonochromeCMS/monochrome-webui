4b3c40f982bdf569d52e6766e1e68ea83300e68d
Carlos Ruiz <d34dplayer@protonmail.com>
2021-10-25 18:45:37 +0200

    Update the User API calls
    Replace `get_all` with `search`

--- src/api/User.ts
+++ src/api/User.ts
@@ -7,16 +7,27 @@ export type Role = 'admin' | 'uploader' | 'user';
 export interface IUser {
   username: string;
   email?: string;
-  role: Role;
 }
 
-export interface UserSchema extends IUser {
+export interface UserRegisterSchema extends IUser {
   password: string;
+}
+
+export interface UserSchema extends UserRegisterSchema {
+  role: Role;
 }
 
 export interface UserResponse extends IUser {
   id: string;
   version: number;
+  role: Role;
+}
+
+export interface UserFilters {
+  username?: string;
+  role?: Role;
+  email?: string;
+  id?: string;
 }
 
 export type UsersResponse = Pagination<UserResponse>;
@@ -28,8 +39,17 @@ export default class User extends Base {
     return role === 'admin';
   }
 
-  public static async get_all(auth: AxiosRequestConfig, limit = 10, offset = 0, delay = false) {
-    const url = `?limit=${limit}&offset=${offset}`;
+  public static async search(
+    auth: AxiosRequestConfig,
+    filters: UserFilters,
+    limit = 10,
+    offset = 0,
+    delay = false,
+  ) {
+    const url = [...Object.entries(filters), ['limit', limit], ['offset', offset]].reduce(
+      (acc, el) => acc + (el[1] !== null ? `&${el[0]}=${el[1]}` : ''),
+      '?',
+    );
     const response = await User._get(url, auth, delay);
 
     const result: ApiResponse<UsersResponse> = User._apiResponse(response.status);
@@ -160,6 +180,60 @@ export default class User extends Base {
         break;
       case 401:
         result.error = 'Please log in again';
+        break;
+      case 422:
+        result.error = 'The data provided is not valid';
+        break;
+      default:
+        result.error = response.data?.detail ?? response.statusText;
+    }
+    return result;
+  }
+
+  public static async register(data: UserRegisterSchema) {
+    const response = await User._post('/register', data, {});
+
+    const result: ApiResponse<UserResponse> = User._apiResponse(response.status);
+
+    switch (response.status) {
+      case 201:
+        result.data = response.data;
+        break;
+      case 400:
+        result.error = 'Username/email already in use';
+        break;
+      case 405:
+        result.error = 'Registering is not available on this server';
+        break;
+      case 422:
+        result.error = 'The data provided is not valid';
+        break;
+      default:
+        result.error = response.data?.detail ?? response.statusText;
+    }
+    return result;
+  }
+
+  public static async uploadAvatar(userId: string, file: File, auth: AxiosRequestConfig) {
+    const form = new FormData();
+    form.append('payload', file);
+
+    const response = await User._put(`/${userId}/avatar`, form, auth, 'multipart/form-data');
+
+    const result: ApiResponse<UserResponse> = User._apiResponse(response.status);
+
+    switch (response.status) {
+      case 200:
+        result.data = response.data;
+        break;
+      case 400:
+        result.error = "The file provided isn't valid";
+        break;
+      case 401:
+        result.error = 'Please log in again';
+        break;
+      case 404:
+        result.error = 'User not found';
         break;
       case 422:
         result.error = 'The data provided is not valid';
