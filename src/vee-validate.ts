3ba3569cbac6ff9246ac65cd8c89927d32c93b9d
Carlos Ruiz <d34dplayer@protonmail.com>
2021-11-19 14:52:30 +0100

    Add max rule to vee-validate

--- src/vee-validate.ts
+++ src/vee-validate.ts
@@ -1,11 +1,13 @@
 import { extend, localize, setInteractionMode } from 'vee-validate';
-import { digits, numeric, regex, required } from 'vee-validate/dist/rules';
+import { digits, max, numeric, regex, required } from 'vee-validate/dist/rules';
 
 setInteractionMode('eager');
 
 extend('required', required);
 
 extend('digits', digits);
+
+extend('max', max);
 
 extend('numeric', numeric);
 
@@ -15,6 +17,7 @@ localize({
   en: {
     messages: {
       digits: '{_field_} requires {length} digits',
+      max: "{_field_} can't be that long",
       numeric: '{_field_} must be a number',
       regex: '{_field_} is not valid',
       required: '{_field_} can not be empty',
@@ -23,6 +26,7 @@ localize({
   fr: {
     messages: {
       digits: '{_field_} a besoin de {length} chiffres',
+      max: '{_field_} ne peut pas être aussi long',
       numeric: '{_field_} doit être un numéro',
       regex: "{_field_} n'est pas valide",
       required: '{_field_} ne peut pas être vide',
