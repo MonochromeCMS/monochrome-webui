62e109ba325a486c4647045b2a5ce9b438994828
Carlos Ruiz <d34dplayer@protonmail.com>
2021-11-19 15:38:28 +0100

    Add email validator

--- src/vee-validate.ts
+++ src/vee-validate.ts
@@ -1,5 +1,5 @@
 import { extend, localize, setInteractionMode } from 'vee-validate';
-import { digits, max, numeric, regex, required } from 'vee-validate/dist/rules';
+import { digits, email, max, numeric, regex, required } from 'vee-validate/dist/rules';
 
 setInteractionMode('eager');
 
@@ -13,10 +13,13 @@ extend('numeric', numeric);
 
 extend('regex', regex);
 
+extend('email', email);
+
 localize({
   en: {
     messages: {
       digits: '{_field_} requires {length} digits',
+      email: '{_field_} must be a valid email',
       max: "{_field_} can't be that long",
       numeric: '{_field_} must be a number',
       regex: '{_field_} is not valid',
@@ -26,6 +29,7 @@ localize({
   fr: {
     messages: {
       digits: '{_field_} a besoin de {length} chiffres',
+      email: '{_field_} doit être un email valide',
       max: '{_field_} ne peut pas être aussi long',
       numeric: '{_field_} doit être un numéro',
       regex: "{_field_} n'est pas valide",
